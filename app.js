5e2b70cda4eafdfcab4c617f658893a143a09af4
Fojan Babaali <fozhanmail@gmail.com>
2021-12-13 20:34:06 -0800

    slider #2 is ready

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ function Slider(slider) {
   let prev;
   let next;
   const slides = slider.querySelector(".slides");
-  const prevButton = document.querySelector(".goToPrev");
-  const nextButton = document.querySelector(".goToNext");
+  const prevButton = slider.querySelector(".goToPrev");
+  const nextButton = slider.querySelector(".goToNext");
   function startSlider() {
     current = slider.querySelector(".current") || slides.firstElementChild;
     prev = current.previousElementSibling || slides.lastElementChild;
@@ -22,23 +22,32 @@ function Slider(slider) {
     next.classList.add("next");
   }
   function move(direction) {
-    const classesToRemove = ['prev', 'current', 'next'];
+    const classesToRemove = ["prev", "current", "next"];
     prev.classList.remove(...classesToRemove);
     current.classList.remove(...classesToRemove);
     next.classList.remove(...classesToRemove);
     if (direction === "back") {
-      [prev, current, next] = [prev.previousElementSibling || slides.lastElementChild, prev, current];
+      [prev, current, next] = [
+        prev.previousElementSibling || slides.lastElementChild,
+        prev,
+        current,
+      ];
     } else {
-      [prev, current, next] = [current, next, next.nextElementSibling || slides.firstElementChild];
+      [prev, current, next] = [
+        current,
+        next,
+        next.nextElementSibling || slides.firstElementChild,
+      ];
     }
     applyClasses();
-
   }
   startSlider();
   applyClasses();
   // Event Listeners
-prevButton.addEventListener('click', ()=> move('back'));
-nextButton.addEventListener('click',move);
+  prevButton.addEventListener("click", () => move("back"));
+  nextButton.addEventListener("click", move);
 }
 
 const slider1 = Slider(document.querySelector(".slider"));
+const slider2 = Slider(document.querySelector(".dog-slider"));
+
